2244d0f2ba06ffc99e4779cb07b6abe70a00b9ba
Cuong <truongcongcuong0302@gmail.com>
2021-09-14 16:26:14 +0700

    fix repo branch

--- src/main/java/com/example/_TruongCongCuong_Spring_data_jpa/entity/Employee.java
+++ src/main/java/com/example/_TruongCongCuong_Spring_data_jpa/entity/Employee.java
@@ -3,6 +3,7 @@ package com.example._TruongCongCuong_Spring_data_jpa.entity;
 import org.hibernate.annotations.GeneratorType;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -11,6 +12,7 @@ import javax.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
 @Table(name = "employee")
 public class Employee {
 
