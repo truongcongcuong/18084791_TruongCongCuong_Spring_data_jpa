7e904665b694f4a16ad24820fba796457fad9764
Cuong <truongcongcuong0302@gmail.com>
2021-09-14 15:50:47 +0700

    Initial commit

---
+++ src/main/java/com/example/_TruongCongCuong_Spring_data_jpa/entity/Employee.java
@@ -0,0 +1,28 @@
+package com.example._TruongCongCuong_Spring_data_jpa.entity;
+
+import org.hibernate.annotations.GeneratorType;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+}
---
+++ src/test/java/com/example/_TruongCongCuong_Spring_data_jpa/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._TruongCongCuong_Spring_data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
